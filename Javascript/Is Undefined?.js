2d7b1bce62a04e0f9b6999f5509f0ec01f892a0a
jpark799 <jason.j.park79@gmail.com>
2019-09-11 17:08:36 +0900

    undefined function

---
+++ Javascript/Is Undefined?.js
@@ -0,0 +1,17 @@
+/* 
+
+Is Undefined?
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+
+SOURCE: https://www.codewars.com/kata/564a6486c2070faee6000011/train/javascript
+
+*/
+
+function isUndefined(value) {
+    if (value === undefined) {
+        return true
+    } else {
+        return false}
+    }
