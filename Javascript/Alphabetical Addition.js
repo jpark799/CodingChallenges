0e8c2ffdcfe3fdae30d875305a3a05328733ba7e
jpark799 <jason.j.park79@gmail.com>
2019-09-13 23:47:41 +0900

    alphabetical addition initial commit

---
+++ Javascript/Alphabetical Addition.js
@@ -0,0 +1,13 @@
+/*
+Your task is to add up letters to one letter.
+
+The function will be given a variable amount of arguments, each one being a letter to add.
+
+Notes:
+Letters will always be lowercase.
+Letters can overflow (see second to last example of the description)
+If no letters are given, the function should return 'z'
+
+SOURCE: https://www.codewars.com/kata/5d50e3914861a500121e1958/train/javascript
+
+*/
