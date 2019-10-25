e45dac3fbabfa07e335ca0fb755375c5be6a0c11
jpark799 <jason.j.park79@gmail.com>
2019-10-25 15:40:25 +0900

    for each loop

---
+++ Javascript/Misc./Use A For Each Loop.js
@@ -0,0 +1,15 @@
+/*
+Using a For Each loop, add all the items in the array and store the total.
+*/
+
+
+
+const numArray = [3, 2, 5, 10, 11, 2, 7]
+let total = 0
+
+
+numArray.forEach(element => {
+    total += element
+});
+
+console.log(total)
